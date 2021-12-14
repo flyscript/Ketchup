aefdb58bd2b57ab5429506e6f2b12e2c20c5b7ec
William Chapman <willchap6@gmail.com>
2021-12-14 16:29:42 -0700

    Preparing pool and game manager

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
-    
+
+    private static List<GameObject> _activeObjects;
+    private static DateTime _timeOfLastObject;
+
     // Start is called before the first frame update
     void Start()
     {
